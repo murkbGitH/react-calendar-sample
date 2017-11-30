7fa6c055e5cfe401de675185c134912e47f76a7f
小林芳樹 <attack_no22001@yahoo.co.jp>
2017-11-30 22:04:21 +0900

    I study react.js

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import styles from './css/Calendar.css';
 
 class Calendar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    let now = new Date()
+    const year = now.getFullYear()
+    const month = now.getMonth()+1
+    const endMonthDate = new Date(now.getFullYear(), month, 0).getDate()
+    const startDateWeek = new Date(now.getFullYear(), month, 1).getDay()
+
+    this.week = ["月","火","水","木","金","土","日"]
+    this.startDateWeek = startDateWeek
+    this.endMonthDate  = endMonthDate
+  }
+ 
   render() {
     const styles = {
       calendar: {
@@ -13,45 +28,45 @@ class Calendar extends Component {
       }
     }
 
-    let now = new Date()
-    const year = now.getFullYear()
-    const month = now.getMonth()+1
-    const endMonthDate = new Date(now.getFullYear(), month, 0).getDate()
-    const startDateWeek = new Date(now.getFullYear(), month, 1).getDay()
-
-    
-
-
     let weekHtml = []
-    const week = ["月","火","水","木","金","土","日"]
-    week.forEach((v)=>{
+    this.week.forEach((v)=>{
       weekHtml.push(<td style={styles.calendarCell}>{v}</td>)
     })
-
-    let dateHtml = []
-    for(let i=0; i<endMonthDate; i++) {
-      if((i+1 % 7) == 0) {
-        if(startDateWeek < i) {
-          dateHtml.push(<td style={styles.calendarCell}></td>)
-        }else{
-          dateHtml.push(<td style={styles.calendarCell}>{i+1}</td>)
-        }
-      }
-    }
 
     return (
       <div style={styles.calendar}>
         <p>カレンダーサンプル</p>
         <div className="date"></div>
         <table>
-          <tr>
-            {weekHtml}
-          </tr>
-          {dateHtml}
+          <tbody>
+            <tr>
+              {weekHtml}
+            </tr>
+            {this.renderDateTable()}
+          </tbody>
         </table>
       </div>
     );
   }
+  renderDateTable() {
+    var tr = ""
+    var td = ""
+    for(let i=0; i<this.endMonthDate; i++) {
+      if((i+1 % 7) == 0) {
+        td = this.week.map((v) => {
+          return <td>{i}</td>
+        })
+        tr = <tr>{td}</tr>
+      }else{
+        td = this.week.map((v) => {
+          return <td>{i}</td>
+        })
+        tr = td
+      }
+    }
+    return tr
+  }
+ 
 
   
 }
