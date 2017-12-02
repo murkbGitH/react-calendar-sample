2a051e194c20e60b92463022e89a39c5618de30b
YoshikiKobayashi <attack_no22001@yahoo.co.jp>
2017-12-02 13:24:09 +0900

    It is simple calndar!!

--- src/Calendar.js
+++ src/Calendar.js
@@ -9,10 +9,10 @@ class Calendar extends Component {
     let now = new Date()
     const year = now.getFullYear()
     const month = now.getMonth()+1
-    const endMonthDate = new Date(now.getFullYear(), month, 0).getDate()
-    const startDateWeek = new Date(now.getFullYear(), month, 1).getDay()
+    const endMonthDate = new Date(now.getFullYear(), month-1, 0).getDate()
+    const startDateWeek = new Date(now.getFullYear(), month-1, 1).getDay()
 
-    this.week = ["月","火","水","木","金","土","日"]
+    this.week = ["日","月","火","水","木","金","土"]
     this.startDateWeek = startDateWeek
     this.endMonthDate  = endMonthDate
   }
@@ -49,26 +49,29 @@ class Calendar extends Component {
     );
   }
   renderDateTable() {
-    var tr = ""
-    var td = ""
-    for(let i=0; i<this.endMonthDate; i++) {
-      if((i+1 % 7) == 0) {
-        td = this.week.map((v) => {
-          return <td>{i}</td>
-        })
-        tr = <tr>{td}</tr>
-      }else{
-        td = this.week.map((v) => {
-          return <td>{i}</td>
-        })
-        tr = td
-      }
+    let tr = []
+    let td = []
+    let tds = []
+    let calendarRow = Math.ceil(this.endMonthDate / 7) + this.startDateWeek
+    let date = 0
+    console.log(calendarRow)
+    for(let i=0; i<calendarRow; i++) {
+      tds = this.week.map((v, index) => {
+        //カレンダー1行目
+        if(i === 0 && index < this.startDateWeek) {
+          return(<td></td>)
+        }else if(date > this.endMonthDate) {
+          date += 1
+          return(<td></td>)
+        }else{
+          date += 1
+          return(<td>{date}</td>)
+        }
+      })
+      tr.push(<tr>{tds}</tr>)
     }
     return tr
   }
- 
-
-  
 }
 
 export default Calendar;
